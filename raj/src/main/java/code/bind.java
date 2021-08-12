dbb8ed7093006db57e553f7500532f159f5ae334
rajsekhar <rajsekhar.ju@gmail.com>
2021-08-12 20:01:37 +0530

    1st commit

---
+++ raj/src/main/java/code/bind.java
@@ -0,0 +1,9 @@
+package code;
+
+public class bind {
+	
+	public String Sample() {
+		return  "sample";
+	}
+
+}
---
+++ raj/src/test/java/com/codebind/Apptest.java
@@ -0,0 +1,13 @@
+package com.codebind;
+
+import org.testng.annotations.Test;
+
+public class Apptest {
+	@Test
+	public void test() {
+		
+	System.out.println("hello world");
+	}
+	
+
+}
